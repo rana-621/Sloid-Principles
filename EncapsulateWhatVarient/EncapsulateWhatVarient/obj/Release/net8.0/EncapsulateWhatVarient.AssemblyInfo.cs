2e83dbc93f0f1c7396c879545eb3e9da2b09270b
rana-621 <ranaebrahim621@gmail.com>
2025-02-07 19:17:53 +0200

    delete file .gitignore

--- EncapsulateWhatVarient/EncapsulateWhatVarient/obj/Release/net8.0/EncapsulateWhatVarient.AssemblyInfo.cs
+++ EncapsulateWhatVarient/EncapsulateWhatVarient/obj/Release/net8.0/EncapsulateWhatVarient.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EncapsulateWhatVarient")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+909bf40e50a1c42c36b0bc669dd54e3da42de553")]
 [assembly: System.Reflection.AssemblyProductAttribute("EncapsulateWhatVarient")]
